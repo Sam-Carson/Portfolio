d20bfb9b24ee643ed3484d5bea5049bd62184684
samcarson <carsonsam4@gmail.com>
2021-03-02 13:34:53 -0800

    Created the fade in effect for sections

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -2,6 +2,32 @@
 const hamburger = document.getElementsByClassName('hamburger-container');
 const mobileNavSlideOut = document.getElementsByClassName('mobile-nav-slide-out');
 
-hamburger.addEventListener('click', () => {
 
-})
+
+
+
+// fade in effect
+const fadeIn = document.querySelectorAll('.fade-up');
+let viewportHeight = self.innerHeight;
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -150px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+    (function (entries, appearOnScroll) { 
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('appear');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+fadeIn.forEach(fadeIn => {
+    appearOnScroll.observe(fadeIn);
+});
+
